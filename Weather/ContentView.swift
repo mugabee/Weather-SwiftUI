9e4d43fb44235935c3fe8d0607e063e859cdde6c
mugabe richard <mugabe.richard.37@gmail.com>
2021-07-12 13:17:28 +0200

    Project Creation and the Kick-Off

---
+++ Weather/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather
+//
+//  Created by MUGABE RICHARD on 12/07/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Weather/WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherApp.swift
+//  Weather
+//
+//  Created by MUGABE RICHARD on 12/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
